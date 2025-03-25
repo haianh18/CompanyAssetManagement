18114184b6e6d7e8356fd986b3a202622e1fc931
haianh18 <101784211+haianh18@users.noreply.github.com>
2025-03-25 08:09:08 +0700

    add BorrowRequestController

---
+++ FinalProject/Controllers/BorrowRequestController.cs
@@ -0,0 +1,39 @@
+﻿using FinalProject.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class BorrowRequestController : Controller
+    {
+        private readonly IBorrowTicketService _borrowTicketService;
+        public async Task<IActionResult> BorrowRequests()
+        {
+            var requests = await _borrowTicketService.GetBorrowTicketsWithoutReturnAsync();
+            return View(requests);
+        }
+
+        public async Task<IActionResult> ApproveBorrowRequest(int id)
+        {
+            var request = await _borrowTicketService.GetByIdAsync(id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+            // Logic to approve borrow request
+            // This might involve changing status, updating warehouse assets, etc.
+            return RedirectToAction(nameof(BorrowRequests));
+        }
+
+        public async Task<IActionResult> RejectBorrowRequest(int id)
+        {
+            var request = await _borrowTicketService.GetByIdAsync(id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+            // Logic to reject borrow request
+            // This might involve changing status, etc.
+            return RedirectToAction(nameof(BorrowRequests));
+        }
+    }
+}
